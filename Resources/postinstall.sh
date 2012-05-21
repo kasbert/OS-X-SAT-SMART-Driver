e2526f893e5a5fa6c1210b1b2f9d44f56208d625
Jarkko Sonninen <kasper@iki.fi>
2012-05-21 22:29:52 +0300

    Merge from https://github.com/RJVB/OS-X-SAT-SMART-Driver Version 0.5.

--- Resources/postinstall.sh
+++ Resources/postinstall.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 chmod -R go-w /System/Library/Extensions/SATSMARTDriver.kext /System/Library/Extensions/SATSMARTLib.plugin
 chown -R root:wheel /System/Library/Extensions/SATSMARTDriver.kext /System/Library/Extensions/SATSMARTLib.plugin
+kextutil /System/Library/Extensions/SATSMARTDriver.kext
+exit 0
