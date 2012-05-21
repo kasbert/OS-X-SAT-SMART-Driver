35b98eef405e3d57a90e6a46b4135d96a466ea5d
RJVB <bertin@portia.local>
2012-05-21 12:28:04 +0200

    tweaks to build chain

--- build.sh
+++ build.sh
@@ -6,18 +6,19 @@ rm -rf SATSMARTDriver/build
 rm -rf Root
 mkdir -p Root/System/Library/Extensions/
 
-cp -v -r SATSMARTDriver/build/Release/SATSMARTDriver.kext Root/System/Library/Extensions/
-cp -v -r SATSMARTDriver/build/Release/SATSMARTLib.plugin Root/System/Library/Extensions/
+ditto --rsrc SATSMARTDriver/build/Release/SATSMARTDriver.kext Root/System/Library/Extensions/SATSMARTDriver.kext
+ditto --rsrc SATSMARTDriver/build/Release/SATSMARTLib.plugin Root/System/Library/Extensions/SATSMARTLib.plugin
 
 rm -fr satsmartdriver.pkg
 
-/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker --doc SATSMARTDriver.pmdoc
+source=SATSMARTDriver.pkg
+
+/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker --doc SATSMARTDriver.pmdoc --out "${source}"
 
 #
 size=550
 title="SATSMARTDriver"
-source=SATSMARTDriver.pkg
-finalDMGName=SATSMARTDriver-0.4.dmg
+finalDMGName=SATSMARTDriver-0.41.dmg
 applicationName=applicationName
  
 #Create a R/W DMG. It must be larger than the result will be. In this example, the bash variable "size" contains the size in Kb and the contents of the folder in the "source" bash variable will be copied into the DMG:
@@ -36,24 +37,25 @@ sleep 3
 #Use AppleScript to set the visual styles (name of .app must be in bash variable "applicationName", use variables for the other properties as needed):
 
 echo '
+   activate application "Finder"
    tell application "Finder"
      tell disk "'${title}'"
            open
            set current view of container window to icon view
-           set toolbar visible of container window to false
-           set statusbar visible of container window to false
+           set toolbar visible of container window to true
+           set statusbar visible of container window to true
            set the bounds of container window to {400, 100, 885, 430}
            set theViewOptions to the icon view options of container window
            set arrangement of theViewOptions to not arranged
            set icon size of theViewOptions to 72
-close
-open
-           update without registering applications
-           delay 5
+           close
      end tell
    end tell
 ' | osascript
 
+#           open
+#           update without registering applications
+#           delay 5
 #           eject
 
 #           make new alias file at container window to POSIX file "/Applications" with properties {name:"Applications"}
@@ -68,33 +70,6 @@ sync
 sync
 hdiutil detach ${device}
 hdiutil convert "pack.temp.dmg" -format UDZO -imagekey zlib-level=9 -o "${finalDMGName}"
-rm -f pack.temp.dmg 
+rm -rf pack.temp.dmg  "${source}" Root
 
 exit 0
-
-# Create dmg
-VOL="SATSMARTDriver"
-VER="0.4"
-FILES="SATSMARTDriver.pkg"
-
-DMG="tmp-$VOL.dmg"
-
-# create temporary disk image and format, ejecting when done
-SIZE=`du -sk ${FILES} | sed -n '/^[0-9]*/s/([0-9]*).*/1/p'`
-SIZE=$((${SIZE}/1000+1))
-#hdiutil create "$DMG" -megabytes ${SIZE} -ov -type UDIF
-hdiutil create "$DMG" -megabytes ${SIZE} -ov -type UDIF -fs HFS+
-DISK=`hdid "$DMG" | sed -ne ' /Apple_partition_scheme/ s|^/dev/([^ ]*).*$|1|p'`
-#newfs_hfs -v "$VOL" /dev/r${DISK}s2
-hdiutil eject $DISK
-
-# mount and copy files onto volume
-hdid "$DMG"
-cp -R "${FILES}" "/Volumes/$VOL"
-hdiutil eject $DISK
-#osascript -e "tell application "Finder" to eject disk "$VOL"" && 
-
-# convert to compressed image, delete temp image
-rm -f "${VOL}-${VER}.dmg"
-hdiutil convert "$DMG" -format UDZO -o "${VOL}-${VER}.dmg"
-rm -f "$DMG"
