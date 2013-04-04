7dd36ae4da5952df30c5053c66307dd3046e8ee6
Jarkko Sonninen <kasper@iki.fi>
2013-04-05 01:53:54 +0300

    Use configuration variable

--- build.sh
+++ build.sh
@@ -1,28 +1,27 @@
 #!/bin/sh
 umask 022
 set -e
+
+title=SATSMARTDriver
+pkg=SATSMARTDriver.pkg
+configuration=Release
+
 rm -rf SATSMARTDriver/build
-(cd SATSMARTDriver; xcodebuild -configuration Release -project SATSMARTDriver.xcodeproj)
+(cd SATSMARTDriver; xcodebuild -configuration $configuration -project SATSMARTDriver.xcodeproj)
 #(cd SATSMARTDriver; xcodebuild -project SATSMARTDriver-macosx10.5.xcodeproj/ -sdk macosx10.5)
+
 rm -rf Root
 mkdir -p Root/System/Library/Extensions/
+ditto --rsrc SATSMARTDriver/build/$configuration/SATSMARTDriver.kext Root/System/Library/Extensions/SATSMARTDriver.kext
+ditto --rsrc SATSMARTDriver/build/$configuration/SATSMARTLib.plugin Root/System/Library/Extensions/SATSMARTLib.plugin
 
-ditto --rsrc SATSMARTDriver/build/Release/SATSMARTDriver.kext Root/System/Library/Extensions/SATSMARTDriver.kext
-ditto --rsrc SATSMARTDriver/build/Release/SATSMARTLib.plugin Root/System/Library/Extensions/SATSMARTLib.plugin
+rm -fr "$pkg"
+/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker --doc SATSMARTDriver.pmdoc --out "$pkg"
 
-rm -fr satsmartdriver.pkg
+version=$(cat SATSMARTDriver/build/$configuration/SATSMARTDriver.kext/Contents/Info.plist | xpath "//string[preceding-sibling::key[1]='CFBundleVersion']/text()")
+[[ $configuration != Release ]] && version="$version-$configuration"
+./mkdmg "$pkg" 550 "$title" $version
 
-source=SATSMARTDriver.pkg
-
-/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker --doc SATSMARTDriver.pmdoc --out "${source}"
-
-#
-version=$(cat SATSMARTDriver/build/Release/SATSMARTDriver.kext/Contents/Info.plist | xpath "//string[preceding-sibling::key[1]='CFBundleVersion']/text()")
-size=550
-title=
-
-./mkdmg "$source" 550 "SATSMARTDriver" $version
-
-rm -rf "$source" Root
+rm -rf "$pkg" Root
 
 exit 0
