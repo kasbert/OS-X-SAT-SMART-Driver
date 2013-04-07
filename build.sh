baabe3e2fec54ac64409803d0f60efd772aec91d
Jarkko Sonninen <kasper@iki.fi>
2013-04-07 13:04:20 +0300

    Bump version

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ umask 022
 set -e
 
 title=SATSMARTDriver
-pkg=SATSMARTDriver.pkg
 configuration=Release
 #configuration=Debug
 
@@ -16,12 +15,13 @@ mkdir -p Root/System/Library/Extensions/
 ditto --rsrc SATSMARTDriver/build/$configuration/SATSMARTDriver.kext Root/System/Library/Extensions/SATSMARTDriver.kext
 ditto --rsrc SATSMARTDriver/build/$configuration/SATSMARTLib.plugin Root/System/Library/Extensions/SATSMARTLib.plugin
 
+version=$(cat SATSMARTDriver/build/$configuration/SATSMARTDriver.kext/Contents/Info.plist | xpath "//string[preceding-sibling::key[1]='CFBundleVersion']/text()")
+pkg=${title}-${version}-${configuration}.pkg
+[[ $configuration != Release ]] && version="$version-$configuration"
+#version="$version-macosx10.5"
 rm -fr "$pkg"
 /Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker --doc SATSMARTDriver.pmdoc --out "$pkg"
 
-version=$(cat SATSMARTDriver/build/$configuration/SATSMARTDriver.kext/Contents/Info.plist | xpath "//string[preceding-sibling::key[1]='CFBundleVersion']/text()")
-[[ $configuration != Release ]] && version="$version-$configuration"
-#version="$version-macosx10.5"
 size=$(du -ks Root | awk '{print $1}')
 size=$((size+100))
 ./mkdmg "$pkg" $size "$title" $version
